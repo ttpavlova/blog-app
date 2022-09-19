194456e58705592f3d42e007e4a011b154e02bac
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-09-19 03:09:40 +0300

    working with spring security

--- src/main/java/com/example/demo2rest/configs/SecurityConfiguration.java
+++ src/main/java/com/example/demo2rest/configs/SecurityConfiguration.java
@@ -5,7 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+import java.security.Principal;
 
 @Configuration
 @EnableWebSecurity
@@ -43,4 +55,41 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .logout()
                 .logoutSuccessUrl("/");
     }
+
+    // In-Memory
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$12$q29eD1ktZN9OalGUOhykz.qwSq7u0Is6pXIP2FGV3rrNMx.M4lWl.")
+                .roles("ADMIN", "USER")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$12$q29eD1ktZN9OalGUOhykz.qwSq7u0Is6pXIP2FGV3rrNMx.M4lWl.")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    // db authentication
+    /*@Bean
+    public JdbcUserDetailsManager users(DataSource dataSource) {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$12$q29eD1ktZN9OalGUOhykz.qwSq7u0Is6pXIP2FGV3rrNMx.M4lWl.")
+                .roles("ADMIN", "USER")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$12$q29eD1ktZN9OalGUOhykz.qwSq7u0Is6pXIP2FGV3rrNMx.M4lWl.")
+                .roles("USER")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(admin);
+        users.createUser(user);
+
+        return users;
+    }*/
 }
