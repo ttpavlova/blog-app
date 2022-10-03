febc7853c047d7114964ba15d63dcd97d2da09ce
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-10-03 18:37:09 +0300

    change flex settings for avatar component

--- src/main/js/components/Avatar.js
+++ src/main/js/components/Avatar.js
@@ -3,9 +3,11 @@ import user_avatar from "../img/user_avatar.png";
 
 function Avatar(props) {
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <img src={user_avatar} className="avatar-img" alt="user avatar" />
-            <p>{props.currentUsername}</p>
+        <div className="d-flex justify-content-center justify-content-md-end">
+            <div className="d-flex flex-column align-items-center">
+                <img src={user_avatar} className="avatar-img" alt="user avatar" />
+                <p>{props.currentUsername}</p>
+            </div>
         </div>
     );
 }
